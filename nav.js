2a38712c964160135dd7c355aadef11fcef239bf
Sally <intelligence753@gmail.com>
2021-02-18 13:37:10 +0800

    first

---
+++ nav.js
@@ -0,0 +1,7 @@
+
+
+const menubtn = document.querySelector('.menubtn');
+const menu = document.querySelector('.menu')
+menubtn.addEventListener('click', () => {
+    menu.classList.toggle("toggle")
+});
